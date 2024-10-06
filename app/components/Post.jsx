c225a36fe51864d3df566bd11caaecaa4b76d32a
atharva2770 <atharvapathak2770@gmail.com>
2024-10-06 10:00:20 +0530

    Final App

--- app/components/Post.jsx
+++ app/components/Post.jsx
@@ -1,9 +1,14 @@
+'use client'
+
+import DeletePostButton from "./DeletePostButton";
+
 export default function Post({ id, title, content, authorName }) {
     return (
         <div style={{ border: '1px solid white', padding: '15px', margin: '10px 0' }}>
             <h3>{authorName}</h3>
             <h4>{title}</h4>
             <p>{content}</p>
+            <DeletePostButton postId={id} />
         </div>
     )
 }
